05c5dfd72760a5fc1f69b60830366b16a509d329
Rodrigo Camargo <rodrigocamargo854@gmail.com>
2022-12-07 07:41:53 -0300

    Gui controls done

--- my-portfolio/main.js
+++ my-portfolio/main.js
@@ -1,4 +1,32 @@
 import * as THREE from "https://unpkg.com/three@0.126.1/build/three.module.js";
+import * as dat from "dat.gui";
+
+//controlling the properties
+const gui = new dat.GUI();
+
+const world = {
+  plane: {
+    width: 10,
+    height: 10,
+    widthSegments: 10,
+    heightSegments: 10,
+  },
+};
+gui.add(world.plane, "width", 1, 20).onChange(() => {
+  generatePlane();
+});
+
+gui.add(world.plane, "height", 1, 20).onChange(() => {
+  generatePlane();
+});
+
+gui.add(world.plane, "heightSegments", 1, 20).onChange(() => {
+  generatePlane();
+});
+
+gui.add(world.plane, "widthSegments", 1, 20).onChange(() => {
+  generatePlane();
+});
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -17,6 +45,24 @@ renderer.setSize(innerWidth, innerHeight);
 renderer.setPixelRatio(devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
+function generatePlane() {
+  planeMesh.geometry.dispose();
+  planeMesh.geometry = new THREE.PlaneGeometry(
+    world.plane.width,
+    world.plane.height,
+    world.plane.widthSegments,
+    world.plane.heightSegments
+  );
+
+  const { array } = planeMesh.geometry.attributes.position;
+  for (let i = 0; i < array.length; i += 3) {
+    const x = array[i];
+    const y = array[i + 1];
+    const z = array[i + 2];
+
+    array[i + 2] = z + Math.random();
+  }
+}
 // const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 // const material = new THREE.MeshBasicMaterial({
 //   color: 0x00ff00,
@@ -27,21 +73,36 @@ document.body.appendChild(renderer.domElement);
 // scene.add(mesh);
 camera.position.z = 5;
 
-const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
-const planeMaterial = new THREE.MeshBasicMaterial({
+const planeGeometry = new THREE.PlaneGeometry(10, 10, 10, 10);
+const planeMaterial = new THREE.MeshPhongMaterial({
   color: 0xff0000,
-  side: THREE.DoubleSide
+  side: THREE.DoubleSide,
+  flatShading: THREE.FlatShading,
 });
 
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(planeMesh);
 
-scene.add(planeMesh)
+const { array } = planeMesh.geometry.attributes.position;
+for (let i = 0; i < array.length; i += 3) {
+  const x = array[i];
+  const y = array[i + 1];
+  const z = array[i + 2];
+
+  array[i + 2] = z + Math.random();
+}
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+
+light.position.set(0, 0, 1);
+scene.add(light);
+
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
-//   mesh.rotation.x += 0.01;
-//   mesh.rotation.y += 0.01;
-  planeMesh.rotation.x += 0.01;
+  //   mesh.rotation.x += 0.01;
+  //   mesh.rotation.y += 0.01;
+  // planeMesh.rotation.x += 0.01;
 }
 
 animate();
